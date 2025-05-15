bdb2f19138a6ba1f3592008f08c758bd1e4a3a2f
Zubairiii01 <zubairirahman123@gmail.com>
2025-05-15 21:57:19 +0700

    slicing awal dashboard mhs

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -16,7 +16,7 @@ class MahasiswaSeeder extends Seeder
     public function run()
     {
         Mahasiswa::create([
-            'id_mahasiswa' => 'MHS007',
+            'id_mahasiswa' => '',
             'nama' => 'Abhipraya12345',
             'nrp' => '3123500051',
             'prodi' => 'Teknik Informatika',
